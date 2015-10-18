ab97afaa1f028690adddaaa141a62bb6eb7d7a1d
Chuck Hays <github@chuckhays.net>
2015-10-18 10:43:01 -0700

    Add mirror script

--- mirror.py
+++ mirror.py
@@ -1,10 +1,23 @@
+#!/usr/bin/python
+import argparse
 import traceback
 
-import smugsync.Smugmug
+from smugsync.Smugmug import Smugmug
 
 
 def main():
-    pass
+    parser = argparse.ArgumentParser(description='SmugMug mirror script')
+    parser.add_argument('-a', '--apikey', type=str, help='api key', required=True)
+    parser.add_argument('-o', '--oauthsecret', type=str, help='oauth secret', required=True)
+    parser.add_argument('-n', '--appname', type=str, help='app name', required=True)
+    parser.add_argument('-p', '--path', type=str, help='output path', required=True)
+
+    args = parser.parse_args()
+    smugmug = Smugmug(api_key=args.apikey, oauth_secret=args.oauthsecret, app_name=args.appname)
+    smugmug.authenticate()
+    user = smugmug.get_authorized_user()
+    albums = smugmug.get_albums(user)
+    smugmug.mirror_albums(albums, args.path)
 
 
 if __name__ == "__main__":
